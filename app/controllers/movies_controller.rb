4438126ff46699f5c2c3cbd296441d134470e799
Ivanna <44304361+ivannarodriguez@users.noreply.github.com>
2023-11-11 02:01:36 +0000

    movies done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,38 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def create
+    # retreive users input from params
+    # add record to movie table
+    # populate each col with user input
+    # save
+    # redirect to /movies rather than rendering a template
+
+    m = Movie.new
+    # m's attributes shoudl correspond to the schema in db/migrate
+    m.title = params["title"]
+    m.year = params["year"]
+    m.duration = params["duration"]
+    m.description = params["description"]
+    m.image = params["image"]
+    m.director_id = params["director_id"]
+
+    m.save
+
+    # instead of rendering we redirect
+    redirect_to("/movies")
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where({ :id => the_id })
+
+    the_movie = matching_records.at(0)
+
+    the_movie.destroy
+
+    redirect_to("/movies")
+  end
 end
